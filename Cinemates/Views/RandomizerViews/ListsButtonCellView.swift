3c872bcc658341f3d2e084531d0ecf92758ffe68
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-07 19:58:21 +0200

    Update ListsButtonCellView.swift comments

--- Cinemates/Views/RandomizerViews/ListsButtonCellView.swift
+++ Cinemates/Views/RandomizerViews/ListsButtonCellView.swift
@@ -8,25 +8,39 @@
 
 import SwiftUI
 
+//function to create the lists buttons
 struct ListsButtonCellView: View {
+    // take film list to change it
     @Binding var randomizerTotalList : [Film]
+    //to know if button is activated
     @Binding var listButton: Bool
+    //Same but for the other 2 buttons
     @Binding var otherListButtonOne : Bool
     @Binding var otherListButtonTwo : Bool
     var buttonName: String
+    // the lists the button is linked too
     var buttonAction : [Film]
+     //Same but for the other 2 buttons
     var otherButtonOneAction : [Film]
     var otherButtonTwoAction : [Film]
+    // the total button list
     var TotalButtonAction : [Film] = filmsDatabase
     var body: some View {
         Button(buttonName) {
+            // if this button is not activated and the other 2 neither
             if listButton == false {
                 if otherListButtonOne == false && otherListButtonTwo == false {
+                    // it removed the list of the total button
+                    //(because if neither o the 3 buttons are activated, its the total which is activated)
                     randomizerTotalList.removeAll()
                 }
+                //else it add the films of this list button to randomizer where there is already the ones of the othr activated buttons
                 randomizerTotalList.append(contentsOf: buttonAction)
+                // the button is activated
                 listButton = true
             } else {
+                // if it is pushed to be desactivated
+                // we clean the randomize list to put it in again the lists of the other buttons still active
                 randomizerTotalList.removeAll()
                 if otherListButtonOne == true {
                     randomizerTotalList.append(contentsOf: otherButtonOneAction)
@@ -35,9 +49,11 @@ struct ListsButtonCellView: View {
                     }
                 } else if otherListButtonTwo == true {
                     randomizerTotalList.append(contentsOf: otherButtonTwoAction)
+                // if none of the 3 are activated, total button activated so we put its list in randomizedlist
                 }else {
                     randomizerTotalList.append(contentsOf: filmsDatabase)
                 }
+                //the button is disactivated
                 listButton = false
                 
             }
@@ -50,8 +66,8 @@ struct ListsButtonCellView: View {
         .background(listButton.self == false ? Color.clear : Color.cinematePurpleLight)
         .cornerRadius(20)
         .overlay(
-            RoundedRectangle(cornerRadius: 20) // Ici, tu définis le rayon des coins
-                .stroke(Color.cinemateGrayDark, lineWidth: 1) // Bordure blanche de 4 points de large
+            RoundedRectangle(cornerRadius: 20) 
+                .stroke(Color.cinemateGrayDark, lineWidth: 1)
         )
     }
 }
