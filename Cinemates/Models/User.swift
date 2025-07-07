3af70f1f6ef93aa5db21b97aee2b725e212f9a4b
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-07 20:20:50 +0200

    Update User.swift

--- Cinemates/Models/User.swift
+++ Cinemates/Models/User.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Carine ESPEJO on 30/01/2025.
 //
-//Its an app POC made by beginners, the teachers couldn't teach us everything to make to work the database
+//Its an app POC made by beginners, the teachers couldn't teach us everything in less than 2 weeks how to have a clean and working database
 
 import Foundation
 import DeveloperToolsSupport
