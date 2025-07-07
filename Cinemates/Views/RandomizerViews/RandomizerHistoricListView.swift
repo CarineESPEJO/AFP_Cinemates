06dea64eec0310cc30fdff5085a7f27b8d6068ae
CarineESPEJO <98638907+CarineESPEJO@users.noreply.github.com>
2025-07-07 20:11:08 +0200

    Update RandomizerHistoricListView.swift comments

--- Cinemates/Views/RandomizerViews/RandomizerHistoricListView.swift
+++ Cinemates/Views/RandomizerViews/RandomizerHistoricListView.swift
@@ -7,17 +7,21 @@
 
 import SwiftUI
 
+// View for film historic list
 struct RandomizerHistoricListView: View {
-    
+
+    // to define the layout of columns of a 2-column vertical grid (.flexible = take all the space it can)
     let columns = [GridItem(.flexible(), spacing: 8), GridItem(.flexible(), spacing: 8)]
+    
     var listShowing: [Film]
     
-    @Environment(\.dismiss) var dismiss // Ajout de dismiss pour fermer la feuille
+    @Environment(\.dismiss) var dismiss // to close the pageView
     
     var body: some View {
         
         VStack(alignment: .leading) {
             HStack {
+                // button to close the pageView
                 Button {
                     dismiss()
                     
@@ -36,7 +40,6 @@ struct RandomizerHistoricListView: View {
                 
                 Spacer ()
                 
-                // Titre
                 Text("Historique du Randomizer")
                     .font(.staatlichesTitle2())
                     .multilineTextAlignment(.center)
@@ -44,7 +47,8 @@ struct RandomizerHistoricListView: View {
                     .padding()
                 
                 Spacer ()
-                
+
+                //to center the title by adding same button but fake and invisible
                 HStack {
                     Image (systemName: "chevron.left")
                         .font(.urbanistSubHeadline())
@@ -53,10 +57,13 @@ struct RandomizerHistoricListView: View {
                 }.foregroundStyle(.cinematePurpleDark)
             }
             .padding(.horizontal, 10)
-            
+
+            //Scroll view to see all the films of the list
             ScrollView {
                 VStack {
+                //Creates a 2-column vertical grid 
                 LazyVGrid(columns: columns, spacing: 16) {
+                    //Each movie of the list appear in the FilmCellView format into the grid
                     ForEach(listShowing) { film in
                         NavigationLink {
                             FilmPageView(FilmP: film)
